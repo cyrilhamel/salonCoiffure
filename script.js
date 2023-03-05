e7a75788c8e21e27110ff0f9fb3709721d6857ff
TornikeTinikashvili <ttttornike@gmail.com>
2023-03-05 22:32:02 +0100

    gallery-container. html, css, js.

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 function maRedirection(lien) {
     location.href = lien;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* tornike */
+let loading = document.querySelector(".loading");
+console.log(loading);
+window.addEventListener("load", (event) => {
+    loading.classList.remove("active");
+});
