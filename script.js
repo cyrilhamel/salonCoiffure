7eaec84e6e6f0c2db0e1fafb4789ad5d3fc9c38c
LeaCasagrande <lea.casagrande27@gmail.com>
2023-03-06 15:19:56 +0100

    maj style

--- script.js
+++ script.js
@@ -3,12 +3,6 @@ function maRedirection(lien) {
 }
 
 
-const burger = document.querySelector('.burger');
-const menu = document.querySelector('.menu');
-
-burger.addEventListener('click', () => {
-  menu.classList.toggle('show');
-});
 
 
 
