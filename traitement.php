5613bd996bb020df76c72102deed05bcc7bbf055
LeaCasagrande <lea.casagrande27@gmail.com>
2023-03-06 18:40:01 +0100

    maj php

--- traitement.php
+++ traitement.php
@@ -1,8 +1,3 @@
 <?php
-if (isset($_POST['message'])) {
-    $retour = mail($_POST['email'], 'Envoi depuis page Contact', $message, $entete);
-    if($retour){
-        echo '<p>Votre message a bien été envoyé.</p>';
-    }
-}
+
 ?>
